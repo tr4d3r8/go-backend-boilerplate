b0b2b9078663d791c4ccf0216d3061990c089031
tr4d3r <Steve.griffin08@gmail.com>
2021-08-24 16:29:57 -0500

    chore: generated stubs for remaining crud on entries - created test case for create entry

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -6,3 +6,23 @@ INSERT INTO entries (
   $1, $2
 )
 RETURNING *;
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE code = $1 LIMIT 1;
+
+-- name: ListEntries :many
+SELECT * FROM entries
+ORDER BY code
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateEntry :one
+UPDATE  entries 
+SET amount = $2
+WHERE code = $1
+RETURNING *;
+
+-- name: DeleteEntry :exec
+DELETE FROM entries
+WHERE code = $1;
---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// for test isolation we will create a random entry for each test
+func createRandomEntry(t *testing.T) Entry {
+	entry1 := createRandomTransfer(t)
+
+	argTo := CreateEntryParams {
+		AccountID: 	entry1.ToAccountID,
+		Amount: 	entry1.Amount,
+	}
+
+	// create entry 
+	entry, err := testQueries.CreateEntry(context.Background(), argTo)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, entry1.Amount, entry.Amount)
+	require.Equal(t, entry1.ToAccountID, entry.AccountID)
+
+	require.NotZero(t, entry.Code)
+	require.NotZero(t, entry.CreatedAt)
+
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
