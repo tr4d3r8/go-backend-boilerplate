621f7283b2f3e310f528e0e0ae0a80f925a83eda
tr4d3r <Steve.griffin08@gmail.com>
2021-08-25 21:25:22 -0500

    feat: added basic api handlers and routes

---
+++ api/server.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "gitlab.com/tr4d3r/backend-master-golang/db/sqlc"
+)
+
+//  serve all http requests
+type Server struct {
+	store 		*db.Store 
+	router		*gin.Engine
+}
+
+// create  new server instance 
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+
+	router.POST("/account", server.createAccount)
+	router.GET("/account/:id", server.getAccount)
+	router.GET("/account", server.listAccount)
+	// add routes to router 
+	
+	server.router = router
+	return server
+}
+
+// runs the http server on specific address and starts listening for requests 
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
