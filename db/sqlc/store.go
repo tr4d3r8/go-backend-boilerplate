e47f464bc3bf58d89ec4a09919cf419b4142b05d
tr4d3r <Steve.griffin08@gmail.com>
2021-08-25 19:28:05 -0500

    refactor: created addMoney func to simplify logic and clean up store.go

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -100,39 +100,9 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		
 		// make sure we always execute transactions in a standard order 
 		if arg.FromAccountID < arg.ToAccountID {
-
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID: 	arg.FromAccountID,
-			Amount: 	-arg.Amount,
-		})
-		if err != nil {
-			return err 
-		}
-
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID: 	arg.ToAccountID,
-			Amount: 	arg.Amount,
-		})
-		if err != nil {
-			return err 
-		}
-	} else {
-
-		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID: 	arg.ToAccountID,
-			Amount: 	arg.Amount,
-		})
-		if err != nil {
-			return err 
-		}
-
-		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-			ID: 	arg.FromAccountID,
-			Amount: 	-arg.Amount,
-		})
-		if err != nil {
-			return err 
-		}
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 
 	}
 
@@ -140,4 +110,27 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 	})
 
 	return result, err
+}
+
+func addMoney(
+	ctx context.Context, 
+	q *Queries,
+	accountID1 int64,
+	amount1 int64, 
+	accountID2 int64, 
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID: accountID2,
+		Amount: amount2,
+	})
+	return
 }
