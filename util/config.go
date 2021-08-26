9e9ff6bfe2a36cc8dce3d7edfcdefd209fb74aeb
tr4d3r <Steve.griffin08@gmail.com>
2021-08-25 21:45:22 -0500

    feat: added .env file and config.go - using viper to read from file or env

---
+++ util/config.go
@@ -0,0 +1,28 @@
+package util
+
+import "github.com/spf13/viper"
+
+// config stores all configurations for the service
+// the valuses are ready by viper from a config file or environment variable
+type Config struct {
+	DBDriver 			string	`mapstructure:"DB_DRIVER`
+	DBSource 			string	`mapstructure:"DB_SOURCE"`
+	ServerAddress 		string	`mapstructure:"SERVER_ADDRESS"`
+}
+
+// reads configuration from a file if path exists or override with env var 
+func LoadConfig(path string) (config Config, err error){
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	
+	return config, err
+}
